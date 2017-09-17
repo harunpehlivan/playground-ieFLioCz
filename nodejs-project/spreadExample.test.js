47227827429203661c4c3b7058635c201364d0e0
Andrew de Rozario <andrew@justintimecoder.com>
2017-09-17 11:42:40 +0100

    adding object spread

--- nodejs-project/spreadExample.test.js
+++ nodejs-project/spreadExample.test.js
@@ -6,8 +6,10 @@ it('should return have correct element at start of array', () =>{
     const arrayToExtend = [2, 3, 4];
     
     const expectedFirstElement = 1;
-
+    //Fix me to pass the test
     const updatedArray = [];
     
+    console.log(updatedArray);
+
     assert.equal(updatedArray[0], expectedFirstElement);
 });
