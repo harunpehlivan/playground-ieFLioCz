d02cbe1d69ff1ef49eb81803674d2873da8c8e4d
Andrew de Rozario <andrew@justintimecoder.com>
2017-09-18 19:41:21 +0100

    fixing bug

--- nodejs-project/templateString.js
+++ nodejs-project/templateString.js
@@ -1,6 +1,6 @@
 
 const company = { companyId: 1, employeeId: 2 };
 
-let url = `/api/company/${companyId}/employee/${employeeId}`;
+let url = `/api/company/${company.companyId}/employee/${company.employeeId}`;
 
 console.log(url);
